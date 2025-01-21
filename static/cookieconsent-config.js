6e3f037c07e53b25d419dfcdd5ab46c04b182037
Yanxiang Zhan <yanxiang@uni-bremen.de>
2025-01-21 16:03:24 +0100

    add privacy statement

---
+++ static/cookieconsent-config.js
@@ -0,0 +1,62 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "bottom left",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "bar",
+            position: "left",
+            equalWeightButtons: false,
+            flipButtons: true
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        }
+    },
+    language: {
+        default: "en",
+        autoDetect: "document",
+        translations: {
+            en: {
+                consentModal: {
+                    title: "Hello roboticists!",
+                    description: "This site uses cookies that are necessary for its operation. No tracking or optional cookies are used. For more details, please see our Privacy Policy.",
+                    acceptAllBtn: "Accept",
+                    acceptNecessaryBtn: "Reject",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"/privacy\">Privacy Policy</a>"
+                },
+                preferencesModal: {
+                    title: "Consent Preferences Center",
+                    acceptAllBtn: "Accept",
+                    acceptNecessaryBtn: "Reject",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Cookie Usage",
+                            description: ""
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "Cookies for managing session with JupyterLab instances.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
